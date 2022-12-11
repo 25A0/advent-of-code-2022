bac0bc0efa21c2a326eaf601859e1ad5e06835dc
Moritz Neikes <moritz@25a0.org>
2022-12-11 22:48:38 +0100

    Day 11 - challenge 1

---
+++ day-11/src/main.rs
@@ -0,0 +1,121 @@
+// The input file contains the signal as a series of characters.
+//
+// Approach:
+// Parse the input file and build a file tree from that. Then iterate
+// through the file tree, and enumerate all nodes that are directories
+// and have a total size of < 10000.
+
+use std::fs::File;
+use std::path::Path;
+use std::io::{self, BufRead};
+
+struct Monkey {
+    items: Vec<usize>,
+    operation: Box<dyn Fn(usize) -> usize>,
+    test: Box<dyn Fn(usize) -> bool>,
+    test_pass_index: usize,
+    test_fail_index: usize,
+}
+
+fn round(monkeys: &mut [Monkey], monkey_stats: &mut [usize]) {
+    for m in 0..monkeys.len() {
+        for i in 0..monkeys[m].items.len() {
+            let mut item = 0;
+            let mut new_monkey_m = 0;
+            { // Restrict scope of mutable monkey reference
+                let mut monkey = &mut monkeys[m];
+                // Remove the item from the front of the vector
+                item = monkey.items.remove(0);
+                // Update the worry level
+                item = (monkey.operation)(item);
+
+                // Relief: divide by three
+                item = item / 3;
+
+                // Test the new worry level
+                new_monkey_m = if (monkey.test)(item) {
+                    monkey.test_pass_index
+                } else {
+                    monkey.test_fail_index
+                };
+
+            }
+
+            // Throw to appropriate monkey
+            monkeys[new_monkey_m].items.push(item);
+
+            // Increment monkey stat
+            monkey_stats[m] = monkey_stats[m] + 1;
+        }
+    }
+}
+
+fn main() {
+    let mut monkeys = [
+        Monkey {
+            items: vec![54, 61, 97, 63, 74],
+            operation: Box::new(|x| x * 7),
+            test: Box::new(|x| x % 117 == 0),
+            test_pass_index: 5,
+            test_fail_index: 3,
+        },
+        Monkey {
+            items: vec![61, 70, 97, 64, 99, 83, 52, 87],
+            operation: Box::new(|x| x + 8),
+            test: Box::new(|x| x % 2 == 0),
+            test_pass_index: 7,
+            test_fail_index: 6,
+        },
+        Monkey {
+            items: vec![60, 67, 80, 65],
+            operation: Box::new(|x| x * 13),
+            test: Box::new(|x| x % 5 == 0),
+            test_pass_index: 1,
+            test_fail_index: 6,
+        },
+        Monkey {
+            items: vec![61, 70, 76, 69, 82, 56],
+            operation: Box::new(|x| x + 7),
+            test: Box::new(|x| x % 3 == 0),
+            test_pass_index: 5,
+            test_fail_index: 2,
+        },
+        Monkey {
+            items: vec![79, 98],
+            operation: Box::new(|x| x + 2),
+            test: Box::new(|x| x % 7 == 0),
+            test_pass_index: 0,
+            test_fail_index: 3,
+        },
+        Monkey {
+            items: vec![72, 79, 55],
+            operation: Box::new(|x| x + 1),
+            test: Box::new(|x| x % 13 == 0),
+            test_pass_index: 2,
+            test_fail_index: 1,
+        },
+        Monkey {
+            items: vec![63],
+            operation: Box::new(|x| x + 4),
+            test: Box::new(|x| x % 19 == 0),
+            test_pass_index: 7,
+            test_fail_index: 4,
+        },
+        Monkey {
+            items: vec![72, 51, 93, 63, 80, 86, 81],
+            operation: Box::new(|x| x * x),
+            test: Box::new(|x| x % 11 == 0),
+            test_pass_index: 0,
+            test_fail_index: 4,
+        },
+    ];
+
+    let mut monkey_stats = [0;8];
+
+    for i in 0..20 {
+        round(&mut monkeys, &mut monkey_stats);
+    }
+    monkey_stats.sort();
+    monkey_stats.reverse();
+    println!("Monkey business: {}", monkey_stats[0] * monkey_stats[1]);
+}
